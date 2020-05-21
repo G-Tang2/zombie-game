4ff2e42bc35c5d5127f9c170c422badb69fc3eee
Kevin Balapitiya <mbal0012@student.monash.edu>
2020-05-22 03:26:23 +1000

    crafting wepons not implemented in zombie limb

--- src/game/ZombieLimb.java
+++ src/game/ZombieLimb.java
@@ -23,6 +23,7 @@ public class ZombieLimb extends WeaponItem {
      */
     public ZombieLimb(String name, char displayChar, int damage, String verb) {
         super(name, displayChar, damage, verb);
+        allowableActions.add(new CraftWeaponAction(this));
     }
 
 }
